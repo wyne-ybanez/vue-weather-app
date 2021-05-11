8c2515194d4f4cb3af185f0ecc239a5d77d837a3
wyne-ybanez <wyne_01@icloud.com>
2021-05-11 13:58:03 +0100

    Add vue template and bg images

---
+++ vue-weather/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app">
+    <main>
+      <div class="search-box">
+        <input
+          type="text"
+          name=""
+          id=""
+          class="search-bar"
+          placeholder="search..."
+        />
+      </div>
+    </main>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      api_key: "a689aa387e68aba199413708fbb2bea9",
+    };
+  },
+};
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
+  font-family: "montserrat", sans-serif;
+}
+
+#app {
+  background-image: url("./assets/cold-bg.jpeg");
+}
+</style>
